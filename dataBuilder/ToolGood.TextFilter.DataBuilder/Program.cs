ad29f8e33ffe58a1e9c93cb43fb2363b9c1d1251
toolgood <toolgood@qq.com>
2023-05-30 19:06:36 +0800

    添加命令行

---
+++ dataBuilder/ToolGood.TextFilter.DataBuilder/Program.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Data.Sqlite;
+using ToolGood.RcxCrypto;
+using ToolGood.TextFilter.DataBuilder2;
+
+namespace ToolGood.TextFilter.DataBuilder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            if (args.Length == 0) {
+                Console.WriteLine("请输入【文件名】及【输出文件夹】！");
+                return;
+            }
+            //SqliteFactory factory = SqliteFactory.Instance;
+            var file = args[0];
+            var outFile = "";
+            if (args.Length > 1) {
+                outFile = args[1];
+            }
+            build(file, outFile);
+
+            Console.WriteLine("Build complete!");
+        }
+        private static void build(string file, string outFile)
+        {
+            if (File.Exists(file) == false) {
+                Console.WriteLine("请选择敏感词库！");
+                return;
+            }
+            var filePath = Path.GetFullPath(file);
+            var outFilePath = Path.GetFullPath(Path.Combine(outFile, @$"TextFilter-{DateTime.Now.ToString("yyyyMMdd")}.data"));
+
+            var bigFile = false;
+            Startup startup = new Startup();
+
+            startup.Init(filePath, "", false, bigFile);
+            startup.Build();
+
+            if (bigFile) {
+                WriteOutFile("temp/out_big.temp", outFilePath);
+            } else {
+                WriteOutFile("temp/out_small.temp", outFilePath);
+            }
+            Console.WriteLine("完成");
+        }
+        private static void WriteOutFile(string path, string outPath)
+        {
+            var bytes = File.ReadAllBytes(path);
+
+            string pwd = "012345679";
+            bytes = CompressionUtil.GzipCompress(bytes);
+
+            bytes = Rcy.Encrypt(bytes, pwd);
+
+            outPath = Path.GetFullPath(outPath);
+            Directory.CreateDirectory(Path.GetDirectoryName(outPath));
+            File.WriteAllBytes(outPath, bytes);
+        }
+
+    }
+}
---
+++ dataBuilder/ToolGood.TextFilter.DataBuilder/Utils/CompressionUtil.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToolGood.TextFilter.DataBuilder
+{
+    /// <summary>
+    /// 压缩
+    /// </summary>
+    public static class CompressionUtil
+    {
+        /// <summary>
+        /// Gzip压缩
+        /// </summary>
+        /// <param name="data">要压缩的字节数组</param>
+        /// <param name="fastest">快速模式</param>
+        /// <returns>压缩后的数组</returns>
+        public static byte[] GzipCompress(byte[] data, bool fastest = false)
+        {
+            if (data == null || data.Length == 0)
+                return data;
+            try {
+                using (MemoryStream stream = new MemoryStream()) {
+                    var level = fastest ? CompressionLevel.Fastest : CompressionLevel.Optimal;
+                    using (GZipStream zStream = new GZipStream(stream, level)) {
+                        zStream.Write(data, 0, data.Length);
+                    }
+                    return stream.ToArray();
+                }
+            } catch {
+                return data;
+            }
+        }
+    }
+}
